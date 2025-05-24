cc62277801d30cf831d6b7180d075a7fc599d4f9
Nguyen Dinh Ban <saomai26011998@gmail.com>
2025-05-24 23:48:01 +0700

    ban-update-5

--- Web-ecommerce-master/admin/src/Pages/Admin/Admin.jsx
+++ Web-ecommerce-master/admin/src/Pages/Admin/Admin.jsx
@@ -5,6 +5,7 @@ import Sidebar from '../../Components/Sidebar/Sidebar'
 import { Routes,Route } from 'react-router-dom'
 import AddProduct from '../../Components/AddProduct/AddProduct'
 import ListProduct from '../../Components/ListProduct/ListProduct'
+import UserList from '../../Components/UserList/UserList'
 
 const Admin = () => {
   return (
@@ -13,7 +14,7 @@ const Admin = () => {
       <Routes>
         <Route path='/addproduct' element={<AddProduct/>} />
         <Route path='/listproduct' element={<ListProduct/>} />
-
+        <Route path='/users' element={<UserList/>} />
       </Routes>
     </div>
   )
