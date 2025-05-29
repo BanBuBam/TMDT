86d8cc8cb4a4de470e54b987749bddb6aa4197fa
Nguyen Dinh Ban <saomai26011998@gmail.com>
2025-05-29 12:36:19 +0700

    ban-update-11

--- Web-ecommerce-master/admin/src/Components/Navbar/Navbar.jsx
+++ Web-ecommerce-master/admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/logo22.png'
 import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src={navlogo} alt="" className="nav-logo" />
+            <div className="nav-brand">
+                <img src={navlogo} alt="" className="nav-logo" />
+                <span className="brand-name">Ride On</span>
+            </div>
             <img src={navProfile} className='nav-profile' alt="" />
         </div>
     )
